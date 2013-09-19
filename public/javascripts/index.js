def635377e1aae8c217381912b849a5d550562f9
dimaxweb <dimitry323@gmail.com>
2013-09-19 10:29:36 +0300

    add homepage to new format

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,185 +1,16 @@
-require.config({
-    baseUrl:'/javascripts/lib',
-    paths:{
-        jquery:[
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
-            //If the CDN location fails, load from this location
-            'jquery1.7.2.min'
-        ],
-
-        jQueryUI:[
-            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min',
-            //If the CDN location fails, load from this location
-            'jquery-ui.min'
-        ],
-
-        swfObject:[
-            'https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject',
-            //If the CDN location fails, load from this location
-            'swfobject'
-        ],
-
-        youTubeLib:'/javascripts/youTubeLib',
-        'jquery.colorbox-min':'/javascripts/lib/jquery.colorbox-min',
-        storage:'/javascripts/storage',
-        geonames:'/javascripts/geonames',
-        search:'/javascripts/search',
-        flickrWidget:'/javascripts/flickrWidget',
-        youtubeWidget:'/javascripts/youtubeWidget',
-        wikiPediaWidget:'/javascripts/wikiPediaWidget',
-        'ajax-scroll':'/javascripts/lib/jquery-paged-scroll.min',
-        flickrLib:'/javascripts/flickrLib',
-        youTubeLib:'/javascripts//youTubeLib',
-        contentWidget:'/javascripts/contentWidget',
-        tooltip:'/javascripts/lib/bootstrap/bootstrap-tooltip',
-        popover:'/javascripts/lib/bootstrap/bootstrap-popover',
-        carousel:'/javascripts/lib/bootstrap/bootstrap-carousel',
-        twitter_grid:'/javascripts/twitter-grid',
-        storiesList : '/javascripts/storiesList',
-        moment : '/javascripts/lib/moment',
-        sharePlugin  : '/javascripts/lib/jquery.sharrre-1.3.4.min',
-        socialShare  : '/javascripts/socialShare',
-        "jquery.ui.autocomplete.html" : '/javascripts/lib/jquery.ui.autocomplete.html',
-        ///css resources
-        paginationCSS:'/stylesheets/pagination',
-        jQueryUICSS:'/stylesheets/jquery-ui-1.8.20.custom',
-        colorBoxCSS:'/stylesheets//colorBox',
-        wikiCSS:'/stylesheets/wiki2',
-        storiesListCss : '/stylesheets/storiesList',
-        css:'/javascripts/lib/css'
-
-
-    },
-    //Order of the preferences
-    shim:{
-        "jQueryUI":{
-            deps:["jquery"]
-        },
-        "youTubeLib":{
-            deps:["jquery"]
-        },
-
-        "flickrLib":{
-            deps:["jquery"]
-        },
-
-        "wikiPediaWidget":{
-            deps:["jquery"]
-        },
-
-        "youtubeWidget":{
-            deps:["jquery"]
-        },
-
-        "flickrWidget":{
-            deps:["jquery"]
-        },
-
-        "jquery.paginate":{
-            deps:["jquery"]
-        },
-
-        "jquery.colorbox-min":{
-            deps:["jquery"]
-        },
-
-        "ajax-scroll":{
-            deps:["jquery"]
-        },
-
-        "storage":{
-            deps:["jquery"]
-        },
-
-        "search":{
-            deps:["jquery", "jQueryUI"]
-        },
-        "geonames":{
-            deps:["jquery"]
-        },
-
-        ///bootstrap
-        "tooltip":{
-            deps:["jquery"]
-        },
-
-        "popover":{
-            deps:["jquery"]
-        },
-
-        "storiesList":{
-            deps : ["jquery"]
-        },
-        "moment":{
-            deps : ["jquery"]
-        },
-        "sharePlugin"  : {
-            deps : ["jquery"]
-        },
-
-        "socialShare" : {
-            deps  : ["jquery","sharePlugin"]
-        },
-
-        "jquery.ui.autocomplete.html" :["jquery","jQueryUI"]
-
-    }
-
-});
-
-
-
 if(typeof(KEENTOUR)==='undefined'){
     KEENTOUR = {};
 }
 
-KEENTOUR.addAddThisWidget = function () {
-    try {
-        window.addthis_config = {};
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.src = 'http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4fe311c477d4f11b';
-        var x = document.getElementsByTagName('head')[0];
-        x.appendChild(s);
-    }
-    catch (e) {
-        //TODO : log here
-    }
 
-}
-require(["storage","geonames","storiesList","socialShare","css!storiesListCss"], function (storage,geonames,storiesList,socialShare) {
+require(["storage","geonames","storiesList","css!storiesListCss"], function (storage,geonames,storiesList) {
 
     KEENTOUR.storage = storage;
-    //KEENTOUR.search = search;
     KEENTOUR.geonames = geonames;
     KEENTOUR.storiesList = storiesList;
-    KEENTOUR.socialShare = socialShare;
-
-
 
 
     $(document).ready(function (e) {
-
-
-        KEENTOUR.socialShare.displayShare('.shareStory');
-
-        /*
-        KEENTOUR.search.bindAutoComplete({
-            container:$('#searchtext'),
-            submitControl:$('#searchbtn'),
-            onItemSelected:function (options) {
-                if(options.geoItem){
-                    var path = KEENTOUR.geonames.getItemUrl(options.geoItem);
-                    window.location = path;
-                }
-                else{
-                    window.location  = "/content/"  + options.searchText;
-                }
-
-            }});
-
-
-         */
 
         KEENTOUR.storiesList.showLatest($('.latestStories'));
 
@@ -192,26 +23,9 @@ require(["storage","geonames","storiesList","socialShare","css!storiesListCss"],
         $('.filter').on('click',function(e){
            $(this).hide();
            $('.filterItems').show();
-        });
+    });
 
-
-
-        /*
-            Add this widget
-        */
-            try {
-                KEENTOUR.addAddThisWidget();
-            }
-            catch (e) {
-                //TODO : enable console logging
-            }
-            var addSenceBottom = $('<script type="text/javascript"><!-- google_ad_client = "ca-pub-4780158497290031"; /* BeetwenRowsLinks */ google_ad_slot = "7565752583"; google_ad_width = 728; google_ad_height = 15; //--> </script> <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"> </script>')
-                .appendTo('.homeVideos');
-
-
-        });
-
-
+    });
 
 
 
--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -1,148 +1,3 @@
-//require.config({
-//    baseUrl:'/javascripts/lib',
-//
-//    paths:{
-//        jquery:[
-//            'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
-//            //If the CDN location fails, load from this location
-//            'jquery1.7.2.min'
-//        ],
-//
-//        jQueryUI:[
-//            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min',
-//            //If the CDN location fails, load from this location
-//            'jquery-ui.min'
-//        ],
-//
-//        swfObject:[
-//            'https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject',
-//            //If the CDN location fails, load from this location
-//            'swfobject'
-//        ],
-//
-//        youTubeLib:'/javascripts/youTubeLib',
-//        'jquery.colorbox-min':'/javascripts/lib/jquery.colorbox-min',
-//        'ajax-scroll':'/javascripts/lib/jquery-paged-scroll.min',
-//        storage:'/javascripts/storage',
-//        geonames:'/javascripts/geonames',
-//        search:'/javascripts/search',
-//        flickrWidget:'/javascripts/flickrWidget',
-//        youtubeWidget:'/javascripts/youtubeWidget',
-//        wikiPediaWidget:'/javascripts/wikiPediaWidget',
-//        flickrLib:'/javascripts/flickrLib',
-//        youTubeLib:'/javascripts//youTubeLib',
-//        contentWidget:'/javascripts/contentWidget',
-//        tooltip:'/javascripts/lib/bootstrap/bootstrap-tooltip',
-//        popover:'/javascripts/lib/bootstrap/bootstrap-popover',
-//        carousel:'/javascripts/lib/bootstrap/bootstrap-carousel',
-//        twitter_grid:'/javascripts/twitter-grid',
-//        richEditorSource  : '/javascripts/lib/wysihtml5/wysihtml5-0.3.0.min',
-//        richEditor  : '/javascripts/lib/wysihtml5/wysihtml5-AMD',
-//        tabs  :  '/javascripts/lib/bootstrap/bootstrap-tab',
-//        moment : '/javascripts/lib/moment',
-//        sharePlugin  : '/javascripts/lib/jquery.sharrre-1.3.4.min',
-//        socialShare  : '/javascripts/socialShare',
-//        /*
-//            css resources
-//        */
-//        paginationCSS:'/stylesheets/pagination',
-//        storyCSS:'/stylesheets/story',
-//        jQueryUICSS:'/stylesheets/jquery-ui-1.8.20.custom',
-//        colorBoxCSS:'/stylesheets/colorBox',
-//        wikiCSS:'/stylesheets/wiki2',
-//        richEditorCSS : '/stylesheets/wysihtml',
-//        css:'/javascripts/lib/css'
-//
-//
-//
-//
-//    },
-//
-//    //Order of the loaded files
-//    shim:{
-//        "jQueryUI":{
-//            deps:["jquery"]
-//        },
-//        "youTubeLib":{
-//            deps:["jquery"]
-//        },
-//
-//        "flickrLib":{
-//            deps:["jquery"]
-//        },
-//
-//        "ajax-scroll":{
-//            deps:["jquery"]
-//        },
-//
-//        "wikiPediaWidget":{
-//            deps:["jquery"]
-//        },
-//
-//        "youtubeWidget":{
-//            deps:["jquery"]
-//        },
-//
-//        "flickrWidget":{
-//            deps:["jquery"]
-//        },
-//
-//        "jquery.paginate":{
-//            deps:["jquery"]
-//        },
-//
-//        "jquery.colorbox-min":{
-//            deps:["jquery"]
-//        },
-//
-//        "storage":{
-//            deps:["jquery"]
-//        },
-//
-//        "search":{
-//            deps:["jquery", "jQueryUI"]
-//        },
-//        "geonames":{
-//            deps:["jquery"]
-//        },
-//
-//        ///bootstrap
-//        "tooltip":{
-//            deps:["jquery"]
-//        },
-//
-//        "popover":{
-//            deps:["jquery","tooltip"]
-//        },
-//
-//        "tabs":{
-//            deps:["jquery"]
-//        },
-//
-//        "moment"  : {
-//            deps : ["jquery"]
-//        },
-//
-//        "sharePlugin"  : {
-//            deps : ["jquery"]
-//        },
-//
-//        "socialShare" : {
-//           deps  : ["jquery","sharePlugin"]
-//        }
-//
-//
-//
-//    }
-//
-//});
-//
-//
-////TODO : think about creating in define or checking findNested dependencies in app.build.js
-////TODO  : refactor move logic to some model
-////TODO  : refactor method get functions
-
-
 if (typeof(KEENTOUR) === "undefined") {
     KEENTOUR = {};
 }
@@ -237,7 +92,7 @@ KEENTOUR.getBigImageUrl = function (photo) {
 }
 
 
-    KEENTOUR.addStoryItem   = function(photo){
+KEENTOUR.addStoryItem   = function(photo){
        var storyContainer = $('.storyItems');
        var item = $("<li class='storyItemLi'><div class='storyItemContainer'><a class='storyPhoto'><img class='imgStory' src='" + KEENTOUR.getBigImageUrl(photo)  +"'/></a></div></li>").appendTo(storyContainer);
        $(item).data('item',photo);
@@ -253,6 +108,7 @@ KEENTOUR.getBigImageUrl = function (photo) {
 
 
 }
+
 
 KEENTOUR.flickrSearch = function (query) {
     //e.preventDefault();
