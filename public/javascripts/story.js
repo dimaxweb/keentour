436ecf91b87691d9197d6e7ad906cd9ac888f2c2
dimaxweb <dimitry323@gmail.com>
2014-01-20 08:16:08 +0200

    get rid from text editor,fix modal and align the layout to center

--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -21,41 +21,41 @@ Array.prototype.getUnique = function(){
 
 KEENTOUR.bindEditor  = function(textareaId) {
 
-    /*
-        Toolbar template
-    */
-
-    var editor = new wysihtml5.Editor(textareaId, {
-        toolbar: "wysihtml5-editor-toolbar",
-        parserRules: wysihtml5ParserRules
-    });
-    //    var log = document.getElementById("log");
+//    /*
+//        Toolbar template
+//    */
 //
-//    editor
-//        .on("load", function() {
-//            log.innerHTML += "<div>load</div>";
-//        })
-//        .on("focus", function() {
-//            log.innerHTML += "<div>focus</div>";
-//        })
-//        .on("blur", function() {
-//            log.innerHTML += "<div>blur</div>";
-//        })
-//        .on("change", function() {
-//            log.innerHTML += "<div>change</div>";
-//        })
-//        .on("paste", function() {
-//            log.innerHTML += "<div>paste</div>";
-//        })
-//        .on("newword:composer", function() {
-//            log.innerHTML += "<div>newword:composer</div>";
-//        })
-//        .on("undo:composer", function() {
-//            log.innerHTML += "<div>undo:composer</div>";
-//        })
-//        .on("redo:composer", function() {
-//            log.innerHTML += "<div>redo:composer</div>";
-//        });
+//    var editor = new wysihtml5.Editor(textareaId, {
+//        toolbar: "wysihtml5-editor-toolbar",
+//        parserRules: wysihtml5ParserRules
+//    });
+//    //    var log = document.getElementById("log");
+////
+////    editor
+////        .on("load", function() {
+////            log.innerHTML += "<div>load</div>";
+////        })
+////        .on("focus", function() {
+////            log.innerHTML += "<div>focus</div>";
+////        })
+////        .on("blur", function() {
+////            log.innerHTML += "<div>blur</div>";
+////        })
+////        .on("change", function() {
+////            log.innerHTML += "<div>change</div>";
+////        })
+////        .on("paste", function() {
+////            log.innerHTML += "<div>paste</div>";
+////        })
+////        .on("newword:composer", function() {
+////            log.innerHTML += "<div>newword:composer</div>";
+////        })
+////        .on("undo:composer", function() {
+////            log.innerHTML += "<div>undo:composer</div>";
+////        })
+////        .on("redo:composer", function() {
+////            log.innerHTML += "<div>redo:composer</div>";
+////        });
 
 };
 
@@ -137,7 +137,7 @@ KEENTOUR.flickrSearch = function (query) {
         showTitle : false,
         showTooltip : false,
         itemCreated : function(domItem,dataItem){
-           var aAppend = $("<a class='addItem'>+</a>").data('photo',dataItem).appendTo(domItem).on("click",function(e){
+           var aAppend = $("<a class='addItem'><img src='../images/plus_btn.png' /></a>").data('photo',dataItem).appendTo(domItem).on("click",function(e){
                e.preventDefault();
                var photo = $(this).data('photo');
                KEENTOUR.addStoryItem(photo);
@@ -411,13 +411,13 @@ require(["storage", "search", "geonames", "flickrWidget","richEditor","jQueryUI"
 
 
 
-        var toolbarTemplate = $('<div id="wysihtml5-editor-toolbar"><header><ul class="commands"><li data-wysihtml5-command="bold" title="Make text bold (CTRL + B)" class="command"></li><li data-wysihtml5-command="italic" title="Make text italic (CTRL + I)" class="command"></li><li data-wysihtml5-command="insertUnorderedList" title="Insert an unordered list" class="command"></li><li data-wysihtml5-command="insertOrderedList" title="Insert an ordered list" class="command"></li><li data-wysihtml5-command="createLink" title="Insert a link" class="command"></li><li data-wysihtml5-command="insertImage" title="Insert an image" class="command"></li><li data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1" title="Insert headline 1" class="command"></li><li data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h2" title="Insert headline 2" class="command"></li><li data-wysihtml5-command-group="foreColor" class="fore-color" title="Color the selected text" class="command"><ul> <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="silver"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="gray"></li><li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="maroon"></li><li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="red"></li><li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="purple"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="green"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="olive"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="navy"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="blue"></li>             </ul>           </li>           <li data-wysihtml5-command="insertSpeech" title="Insert speech" class="command"></li><li data-wysihtml5-action="change_view" title="Show HTML" class="action"></li></ul></header><div data-wysihtml5-dialog="createLink" style="display: none;"><label>Link:<input data-wysihtml5-dialog-field="href" value="http://">         </label>         <a data-wysihtml5-dialog-action="save">OK</a>&nbsp;<a data-wysihtml5-dialog-action="cancel">Cancel</a></div><div data-wysihtml5-dialog="insertImage" style="display: none;">Image:<input data-wysihtml5-dialog-field="src" value="http://"> </label><a data-wysihtml5-dialog-action="save">OK</a>&nbsp;<a data-wysihtml5-dialog-action="cancel">Cancel</a></div></div>')
-            .prependTo('.storyText');
+//        var toolbarTemplate = $('<div id="wysihtml5-editor-toolbar"><header><ul class="commands"><li data-wysihtml5-command="bold" title="Make text bold (CTRL + B)" class="command"></li><li data-wysihtml5-command="italic" title="Make text italic (CTRL + I)" class="command"></li><li data-wysihtml5-command="insertUnorderedList" title="Insert an unordered list" class="command"></li><li data-wysihtml5-command="insertOrderedList" title="Insert an ordered list" class="command"></li><li data-wysihtml5-command="createLink" title="Insert a link" class="command"></li><li data-wysihtml5-command="insertImage" title="Insert an image" class="command"></li><li data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1" title="Insert headline 1" class="command"></li><li data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h2" title="Insert headline 2" class="command"></li><li data-wysihtml5-command-group="foreColor" class="fore-color" title="Color the selected text" class="command"><ul> <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="silver"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="gray"></li><li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="maroon"></li><li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="red"></li><li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="purple"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="green"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="olive"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="navy"></li>               <li data-wysihtml5-command="foreColor" data-wysihtml5-command-value="blue"></li>             </ul>           </li>           <li data-wysihtml5-command="insertSpeech" title="Insert speech" class="command"></li><li data-wysihtml5-action="change_view" title="Show HTML" class="action"></li></ul></header><div data-wysihtml5-dialog="createLink" style="display: none;"><label>Link:<input data-wysihtml5-dialog-field="href" value="http://">         </label>         <a data-wysihtml5-dialog-action="save">OK</a>&nbsp;<a data-wysihtml5-dialog-action="cancel">Cancel</a></div><div data-wysihtml5-dialog="insertImage" style="display: none;">Image:<input data-wysihtml5-dialog-field="src" value="http://"> </label><a data-wysihtml5-dialog-action="save">OK</a>&nbsp;<a data-wysihtml5-dialog-action="cancel">Cancel</a></div></div>')
+//            .prependTo('.storyText');
 
 
 
-        KEENTOUR.bindEditor('description');
-        KEENTOUR.bindEditor('itemTextEditor');
+//        KEENTOUR.bindEditor('description');
+//        KEENTOUR.bindEditor('itemTextEditor');
 
 
         /*
