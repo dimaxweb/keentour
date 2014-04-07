6708b232a5db99a12a232a998618c7c2bccb9fd7
dimaxweb <dimitry323@gmail.com>
2014-04-07 11:36:33 +0300

    add console to global js

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,5 +1,33 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var noop = function noop() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = window.console || {};
+
+    while (length--) {
+        // Only stub undefined methods.
+        console[methods[length]] = console[methods[length]] || noop;
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+
+
 require.config({
     baseUrl:'/javascripts',
+    map: {
+        '*': {
+            css: '../javascripts/lib/css'
+        }
+    },
+
     paths:{
         jquery:'../javascripts/lib/jquery1.7.2.min',
         jQueryUI:'../javascripts/lib/jquery-ui.min',
